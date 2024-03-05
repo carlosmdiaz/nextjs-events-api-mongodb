b69bfcf97340f4f9331918ffec60102a3bbbe0bd
carlosmdiaz <carlosdiaz3979@gmail.com>
2024-03-04 19:26:59 -0500

    In this commit I amde the events database the primary database and added the newsletter collection (it will hold the email registration for the newsletter), and the comments collection (it will hold the comments for each of the events). Also, I connected the comments api route to the database.

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,6 +1,10 @@
 import { buildCommentsPath, extractData, writeToFile } from "../../../helpers/api-util";
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+async function handler(req, res) {
+    const eventId = req.query.eventId;
+    const url = 'mongodb://memelo:Memelo2020@ac-3ryswgt-shard-00-00.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-01.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-02.1bpni6a.mongodb.net:27017/events?ssl=true&replicaSet=atlas-aa944x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=FirstMongoDBProject';
+
     if(req.method === "POST") {
         const email = req.body.email;
         const name = req.body.name;
@@ -18,18 +22,19 @@ function handler(req, res) {
             res.status(422).json({message: 'Invalid Input.'});
             return;
           }
+
         const newComment = {
-            id: new Date().toISOString(),
             email: email,
             name: name,
             text: text,
+            eventId: eventId,
         }
 
-        //Store the Comments in the comments.json
-        const fileData = buildCommentsPath();
-        const data = extractData(fileData);
-        data.push(newComment);
-        writeToFile(fileData, data);
+        const client = await MongoClient.connect(url);
+        const db = client.db();
+        const result = await db.collection('comments').insertOne(newComment);
+        console.log(result);
+        
         res.status(201).json({message: 'Success', comments: newComment})
     }
     if(req.method === 'GET'){
--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 
 async function handler(req, res) {
-    const url = 'mongodb://memelo:Memelo2020@ac-3ryswgt-shard-00-00.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-01.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-02.1bpni6a.mongodb.net:27017/newsletter?ssl=true&replicaSet=atlas-aa944x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=FirstMongoDBProject'
+    const url = 'mongodb://memelo:Memelo2020@ac-3ryswgt-shard-00-00.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-01.1bpni6a.mongodb.net:27017,ac-3ryswgt-shard-00-02.1bpni6a.mongodb.net:27017/events?ssl=true&replicaSet=atlas-aa944x-shard-0&authSource=admin&retryWrites=true&w=majority&appName=FirstMongoDBProject';
     console.log(req)
     if(req.method === 'POST') {
         const email = req.body.email;
@@ -13,7 +13,7 @@ async function handler(req, res) {
 
         const client = await MongoClient.connect(url);
         const db = client.db();
-        await db.collection('emails').insertOne({email: email,});
+        await db.collection('newsletter').insertOne({email: email,});
         client.close();
 
         res.status(201).json({message: "Signed Up!"});
